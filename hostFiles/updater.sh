a98ea1aacd64cef65bdcb5ff92c1b6f26388177d
Anirudh Rath <cyberhybird@hotmail.com>
2020-06-14 07:51:43 +0530

    Developmental Changes - 3
    Added hostFiles Folder to share files

---
+++ hostFiles/updater.sh
@@ -0,0 +1,47 @@
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+NOCOLOR='\033[0m'
+TITLE='\033[0;33m'
+GIT='\033[1;35m'
+echo "Auto-Update"
+cd /usr/local/sbin/
+wget https://mega.nz/file/yewnUCCZ#A4p5MsORg-3ghVb7Pm1PF0EDAesm0oSOfvlzSmHriz8
+
+echo -e "${TITLE}Phase One: ${GREEN}Pre-Configuring Packages${NOCOLOR}"
+sudo dpkg --configure -a
+
+echo
+
+echo -e "${TITLE}Phase Two: ${GREEN}Fix & Attempt Broken Dependencies${NOCOLOR}"
+sudo apt-get install -f
+
+echo
+
+echo -e "${TITLE}Phase Three: ${GREEN}Update Cache${NOCOLOR}"
+sudo apt update
+
+echo
+
+echo -e "${TITLE}Phase Four: ${GREEN}Upgrade Packages${NOCOLOR}"
+sudo apt upgrade
+
+echo
+
+echo -e "${TITLE}Phase Five: ${GREEN}Upgrade Distro${NOCOLOR}"
+sudo apt dist-upgrade
+
+echo
+
+echo -e "${TITLE}Phase Six: ${GREEN}Uninstall Unused Packages${NOCOLOR}"
+sudo apt --purge autoremove
+
+echo
+
+echo -e "${TITLE}Phase Seven: ${GREEN}Clean Up${NOCOLOR}"
+sudo apt autoclean
+
+echo
+
+${GREEN}
+
+echo Complete
