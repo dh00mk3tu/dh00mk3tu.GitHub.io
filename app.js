944a54dfde6125a294a4561005c0ae709dd07c35
Anirudh Rath <cyberhybird@hotmail.com>
2020-08-02 10:05:34 +0530

    Developmental Changes - 5.3
    Removed the ugly ass custom cursor
    Removed page transitionIN from * because it was altering the page particle.js load until page resize

--- app.js
+++ app.js
@@ -2,22 +2,3 @@ particlesJS.load('particles-js', 'particle.json', function() {
   console.log('particles.js loaded - callback');
 });
 
-let mouseCursor = document.querySelector(".cursor");
-let navLinks = document.querySelectorAll('.wrapper');
-
-window.addEventListener('mousemove',cursor);
-
-function cursor(e) {
-  // console.log(e);
-  mouseCursor.style.top = e.pageY + 'px';
-  mouseCursor.style.left = e.pageX + 'px';
-}
-
-navLinks.forEach(link => {
-  link.addEventListener("mouseleave", () => {
-    mouseCursor.classList.remove("cur-grow");
-  });
-  link.addEventListener("mouseover", () => {
-    mouseCursor.classList.add("cur-grow");
-  });
-});
