0eb0d704cf153315d11586e6de97081a02016422
dh00mk3tu <cyberhybird@hotmail.com>
2020-10-04 19:49:06 +0530

    Added 3 projects under Projects tab

--- js/app.js
+++ js/app.js
@@ -2,3 +2,11 @@ particlesJS.load('particles-js', 'particle.json', function() {
   console.log('particles.js loaded - callback');
 });
 
+new Glider(document.querySelector('.glider'), {
+  slidesToShow: 1,
+  draggable: true,
+  arrows: {
+    prev: '.glider-prev',
+    next: '.glider-next'
+  }
+});
