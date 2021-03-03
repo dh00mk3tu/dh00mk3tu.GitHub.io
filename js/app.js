7024da9d90fe4eda8a3a6b0197dacd91ed38141d
dh00mk3tu <cyberhybird@hotmail.com>
2021-03-04 04:40:34 +0530

    Added baffle shuffle>removed mess and garbage code>add SEO meta data

--- js/app.js
+++ js/app.js
@@ -2,11 +2,22 @@ particlesJS.load('particles-js', 'particle.json', function() {
   console.log('particles.js loaded - callback');
 });
 
-new Glider(document.querySelector('.glider'), {
-  slidesToShow: 1,
-  draggable: true,
-  arrows: {
-    prev: '.glider-prev',
-    next: '.glider-next'
-  }
+
+//Shuffle 2
+const b = baffle(".title-sub");
+b.set({
+  characters: '█<▓ ░█▒█▒ ░▒/░█ ▓▒▓ █/██▒ █▓█/ █▒█ ░█▒█ ▓░█▒',
+  speed: 120
 });
+
+var i = 1;
+let changeTitle = () => {
+  let titles = ["Human", "Painter", "Shell Scripter", "Web Developer", "Reverse Engineer", "Programmer", "Artist", "Human?", "Writer", "37-564?"];
+  if (i == titles.length) {
+    i = 0;
+  }
+  b.text(currentText => titles[i]).reveal(1000);
+  i++;
+}
+
+setInterval(changeTitle, 3200);
